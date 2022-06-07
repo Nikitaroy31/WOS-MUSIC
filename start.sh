3ab62b9d1645a6573d0ca0f479164fc4c2898819
Nikitaroy31 <92322219+Nikitaroy31@users.noreply.github.com>
2022-06-07 11:51:44 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+# @nikitabots #
+
+echo "◦•●◉✿ 𝕎𝕆𝕊-𝕄𝕌𝕊𝕀ℂ ✿◉●•◦"
+echo "◦•●◉✿ 𝔻𝕆𝕎ℕ𝕃𝕆𝔸𝔻𝕀ℕ𝔾 𝕎𝕆𝕊-𝕄𝕌𝕊𝕀ℂ ✿◉●•◦"
+git clone https://github.com/Nikitaroy31/WOS-MUSIC-
+echo "◦•●◉✿ 𝔻𝕆𝕎ℕ𝕃𝕆𝔸𝔻𝕀ℕ𝔾 𝕎𝕆𝕊-𝕄𝕌𝕊𝕀ℂ ℝ𝔼ℚ𝕌𝕀ℝ𝔼𝕄𝔼ℕ𝕋𝕊 ✿◉●•◦"
+cd /WOS-MUSIC
+pip3 install -U -r requirements.txt
+echo "◦•●◉✿ 𝕊𝕋𝔸ℝ𝕋𝕀ℕ𝔾 𝕎𝕆𝕊-𝕄𝕌𝕊𝕀ℂ ✿◉●•◦"
+clear
+echo "◦•●◉✿ 𝕊𝕌ℂℂ𝔼𝕊𝕊𝔽𝕌𝕃𝕃𝕐 𝔻𝔼ℙ𝕃𝕆𝕐𝔼𝔻 𝕎𝕆𝕊-𝕄𝕌𝕊𝕀ℂ ✿◉●•◦"
+python3 main.py
